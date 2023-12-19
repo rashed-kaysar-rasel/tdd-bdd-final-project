b4e60ea19602d7a935e78ad244318a3837dd2518
Rashed Kaysar <rashedkaysar321@gmail.com>
2023-12-20 01:11:39 +0600

    done

--- tests/factories.py
+++ tests/factories.py
@@ -32,3 +32,37 @@ class ProductFactory(factory.Factory):
 
     id = factory.Sequence(lambda n: n)
    ## Add code to create Fake Products 
+   from factory import Factory, Faker, FuzzyChoice, FuzzyDecimal
+    class Meta:
+         """Maps factory to data model"""
+        model = Product
+        
+    id = factory.Sequence(lambda n: n)
+    name = FuzzyChoice(
+        choices=[
+            'Hat', 
+            'Pants', 
+            'Shirt',
+            'Apple', 
+            'Banana', 
+            'Pots', 
+            'Towels', 
+            'Ford', 
+            'Chevy', 
+            'Hammer', 
+            'Wrench'
+        ]
+    )
+    description = Faker('text')
+    price = FuzzyDecimal(0.5, 2000.0, 2)
+    available = FuzzyChoice(choices=[True, False])
+    category = FuzzyChoice(
+        choices=[
+            'UNKNOWN', 
+            'CLOTHS',
+            'FOOD', 
+            'HOUSEWARES', 
+            'AUTOMOTIVE', 
+            'TOOLS'
+        ]
+    )
